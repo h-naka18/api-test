37ec2ed36743f6f18bde066dfc1361c8b000b008
Hitoshi <h.naka18@gmail.com>
2024-01-20 20:15:33 +0900

    ♻️ 各アイコンにリンクを設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,42 +40,70 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </a>
         <p className='text-2xl'>　＋　</p>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="https://prismalens.vercel.app/header/logo-dark.svg"
-          alt="prisma_logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://www.prisma.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="https://prismalens.vercel.app/header/logo-dark.svg"
+            alt="prisma_logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </a>
         <p className='text-2xl'>　＋　</p>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/supabase-logo-wordmark--dark.png"
-          alt="supabase_logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://supabase.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/supabase-logo-wordmark--dark.png"
+            alt="supabase_logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </a>
         <p className='text-2xl'>　＋　</p>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://vercel.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </a>
       </div>
 
       <div className="w-full flex flex-col justify-center items-center">
